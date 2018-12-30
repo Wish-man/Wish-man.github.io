761de515a33cf4378d1487539efa14b6ad6d9ed2
Wish-man <46254687+Wish-man@users.noreply.github.com>
2018-12-31 02:52:35 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+   $(document).on('click','.headername',function(){
+        $('.headrname').css('color','red');
+   });
+
+   $(".top_perfume").fadeOut(1500);
+   $(".top_perfume").fadeIn(1500);
+
+});
