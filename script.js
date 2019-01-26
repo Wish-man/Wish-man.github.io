2eb464aa970568c1eb36258ea01012583fb86a4f
Wish-man <46254687+Wish-man@users.noreply.github.com>
2019-01-26 17:31:23 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   $(document).on('click','.headername',function(){
-        $('.headrname').css('color','red');
-   });
-
-   $(".top_perfume").fadeOut(1500);
-   $(".top_perfume").fadeIn(1500);
-
-});
